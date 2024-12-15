17b58c69927fe1313cb0b31789e374b319bd5e8d
abcdefghuy <nguyensanghuypy@gmail.com>
2024-12-15 15:06:28 +0700

    Update .gitignore and constant.java to ignore config.properties

--- src/main/java/org/example/ecommercewebsite/constant/constant.java
+++ src/main/java/org/example/ecommercewebsite/constant/constant.java
@@ -1,9 +1,31 @@
 package org.example.ecommercewebsite.constant;
 
-public class constant {
-    public static final String GOOGLE_CLIENT_ID = "";
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
-    public static final String GOOGLE_CLIENT_SECRET = "";
+public class constant {
+    private static final Properties properties = new Properties();
+
+    // Các hằng số sẽ được gán từ file cấu hình
+    public static String GOOGLE_CLIENT_ID;
+    public static String GOOGLE_CLIENT_SECRET;
+
+    static {
+        try {
+            // Tải file properties từ hệ thống (tệp config.properties trong thư mục gốc của dự án)
+            FileInputStream inputStream = new FileInputStream("config.properties");
+            properties.load(inputStream);
+
+            // Đọc các giá trị từ file cấu hình và gán vào các hằng số
+            GOOGLE_CLIENT_ID = properties.getProperty("GOOGLE_CLIENT_ID");
+            GOOGLE_CLIENT_SECRET = properties.getProperty("GOOGLE_CLIENT_SECRET");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static final String GOOGLE_REDIRECT_URI = "http://localhost:8080/loginGG";
 
